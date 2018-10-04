fe6f944a68cb3b668209cbb8211abe823ceedc61
AbrahamAlzobi <a.alzobi@share.epsb.ca>
2018-10-04 13:23:23 -0600

    update

---
+++ greeting/index.js
@@ -0,0 +1,10 @@
+let hour = 06 || 11; //Change this simulating automatically reading time from Internet Clock or System Tray Clock
+if (hour >= 06); console.log('Good Morning');
+
+
+
+
+
+else if () console.log('Good Afternoon');
+else if () console.log('Good Afternoon');
+else console.log();
